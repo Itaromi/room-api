f343abd692d7597f343ba5eefa488f8f645263e0
ltrentesaux <leo.trentesaux53@gmail.com>
2025-06-30 11:31:18 +0200

    implement authentication and user management with Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Happy developing ✨')
+// src/index.ts
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import authRouter from './routes/auth.routes'; // <-- 1. Importer le routeur
+import userRouter from "./routes/user.routes";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Bienvenue sur l\'API de WorkEase !');
+});
+
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+
+app.listen(port, () => {
+    console.log(`[server]: Serveur démarré sur http://localhost:${port}`);
+});
